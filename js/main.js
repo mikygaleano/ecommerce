2af48e387b1988bfaea6aa3ec22d3c3a12b3c3c5
mikygaleano <creatormikeonline@gmail.com>
2022-12-02 16:37:43 -0300

    ecommerce

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
 
 
-/* Variables */
-const $navbarEmail = document.querySelector('.navbar-email'),
-$desktopMenu = document.querySelector('.desktop-menu'),
-$iconoHamburguer = document.querySelector('.menu'),
-$mobileMenu = document.querySelector('.mobile-menu')
 
-/* funciones */
-function hamToggle () {
-    $mobileMenu.classList.toggle('active');
-};
 
-function navbarEmailToggle () {
-    $desktopMenu.classList.toggle('active');
-};
 
-function navbarEmail () {
-    $navbarEmail.addEventListener('click', navbarEmailToggle );
-};
 
-function hamburguerMenu () {
-    $iconoHamburguer.addEventListener('click', hamToggle);
-};
-
-/* llamados */
-navbarEmail();
-
-hamburguerMenu();
