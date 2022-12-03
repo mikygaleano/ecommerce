0058028097040a9754563ff43dc53f95d859784b
mikygaleano <creatormikeonline@gmail.com>
2022-12-03 00:44:04 -0300

    ecommerce

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 
+const d = document;
+/* variables DOM */
+const $mailMenu = d.querySelector('.navbar-email'),
+$desktopMenuEmail = d.querySelector('.desktop-menu'),
+$arrayBottom = d.querySelector('.array-bottom'),
+$menuBurger = d.querySelector('.menu'),
+$menuMobile = d.querySelector('.mobile-menu'),
+$Carrito = d.querySelector('.navbar-shopping-cart'),
+$productosDetalles = d.querySelector('.product-detail')
 
 
 
+/* funciones */
+function menuEmailOpen () {
+    $desktopMenuEmail.classList.toggle('toggle-top');
+    if ($desktopMenuEmail.classList.contains('toggle-top')) {
+        $productosDetalles.classList.remove('active')
+    }
+};
 
+function menuHamBurger () {
+    $menuMobile.classList.toggle('hamOpen')
+    if ($menuMobile.classList.contains('hamOpen')) {
+        $productosDetalles.classList.remove('active')
+    }
+};
 
+function carritoMenu () {
+    $productosDetalles.classList.toggle('active')
+    if ($productosDetalles.classList.contains('active')) {
+        $desktopMenuEmail.classList.remove('toggle-top');
+        $menuMobile.classList.remove('hamOpen')
+    }
+};
+
+$mailMenu.addEventListener('click', menuEmailOpen);
+$arrayBottom.addEventListener('click', menuEmailOpen);
+
+$menuBurger.addEventListener('click', menuHamBurger);
+
+$Carrito.addEventListener('click', carritoMenu);
